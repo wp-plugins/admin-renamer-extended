479727ac0c6a5ee7ed1025f0231d81f22e14a23a
ramon fincken <ramon fincken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-28 18:59:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/admin-renamer-extended/tags/1.2@158679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -42,7 +42,6 @@ if (count($_POST)) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
-<link href="../styles/style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
