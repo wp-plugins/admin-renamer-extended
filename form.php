2db6dfbc7f51dda6e81ea01a8f0ef1d50acf505c
ramon fincken <ramon fincken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-01 17:41:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/admin-renamer-extended/trunk@159749 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Admin renamer extended operations.
  *
@@ -10,7 +11,7 @@
  * @copyright     Yes, Open source, WebsiteFreelancers.nl
  * @version       v 1.0  26-09-2009 Ramon$
 */
-global $wpdb,$current_user;
+global $wpdb, $current_user;
 
 $message = null;
 $showMsg = 'none';
@@ -19,23 +20,36 @@ $showMsg = 'none';
  * If submiting the form
  */
 if (count($_POST)) {
-   $oldName = htmlspecialchars(trim($_POST['plugin_admin_renamer-oldname'.intval($_GET['user_id'])]));
-   $newName = htmlspecialchars(trim($_POST['plugin_admin_renamer-newname'.intval($_GET['user_id'])]));
+	// Is magic quotes on?
+	if (get_magic_quotes_gpc()) {
+		// Yes? Strip the added slashes
+		$_POST = array_map('stripslashes', $_POST);
+	}
 
-   if(empty($newName))
-   {
-      $message = $oldName. ' NOT changed, new name was empty!';
-      $showMsg = 'block';
-   }
-   else
-   {
-      $user_query = "UPDATE $wpdb->users SET user_login = '{$newName}' WHERE {$wpdb->users}.ID = ".intval($_GET['user_id'])." LIMIT 1";
-      $wpdb->get_results( $user_query );
-      $message = 'Changed '.$oldName. ' into '.$newName;
-      $showMsg = 'block';
-   }
+	$oldName = htmlspecialchars(trim($_POST['plugin_admin_renamer-oldname' . intval($_GET['user_id'])]));
+	$newName = htmlspecialchars(trim($_POST['plugin_admin_renamer-newname' . intval($_GET['user_id'])]));
+
+	if (empty ($newName)) {
+		$message = stripslashes($oldName) . ' NOT changed-> New name was empty!';
+		$showMsg = 'block';
+	} else {
+		if (!validate_username($newName)) {
+			$message = stripslashes($oldName) . ' NOT changed -> This username is invalid. Please enter a valid username.';
+			$showMsg = 'block';
+		} else {
+			if (username_exists($newName)) {
+				$message = stripslashes($oldName) . ' NOT changed -> This username is already registered. Please choose another one.';
+				$showMsg = 'block';
+			} else {
+				$user_query = "UPDATE $wpdb->users SET user_login = '" . mysql_real_escape_string(stripslashes($newName)) . "' WHERE {$wpdb->users}.ID = " . intval($_GET['user_id']) . " LIMIT 1";
+				$wpdb->get_results($user_query);
+				$message = 'Changed ' . stripslashes($oldName) . ' into ' . stripslashes($newName);
+				$showMsg = 'block';
+
+			}
+		}
+	}
 }
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -59,24 +73,23 @@ if (count($_POST)) {
             <?php echo @$message; ?>
          </div>
         </div>
-<?php        
-   $user_query = "SELECT user_id, user_id AS ID, user_login, display_name, user_email, meta_value FROM $wpdb->users, $wpdb->usermeta WHERE {$wpdb->users}.ID = {$wpdb->usermeta}.user_id AND meta_key = '{$wpdb->prefix}capabilities' AND (ID = 1 OR user_login = 'admin' OR meta_value LIKE('%administrator%')) ORDER BY ID ASC";
-   $users = $wpdb->get_results( $user_query );
-   foreach($users AS $row)
-   {
+<?php
+
+$user_query = "SELECT user_id, user_id AS ID, user_login, display_name, user_email, meta_value FROM $wpdb->users, $wpdb->usermeta WHERE {$wpdb->users}.ID = {$wpdb->usermeta}.user_id AND meta_key = '{$wpdb->prefix}capabilities' AND (ID = 1 OR user_login = 'admin' OR meta_value LIKE('%administrator%')) ORDER BY ID ASC";
+$users = $wpdb->get_results($user_query);
+foreach ($users AS $row) {
 ?>        <div class="plugin_admin_renamer-formarea-outer">
            <div class="plugin_admin_renamer-formarea">
            <p>Current admin username: <strong><?php echo $row->user_login; ?></strong>
            <?php
-           if($row->ID == 1)
-           {
-               echo '<br/>Warning: <strong>This is the MAIN admin or site owner!</strong>';
-           }
-           if($row->ID == $current_user->ID)
-           {
-               echo '<br/>Warning: <strong>This is you! After changing your login name you might need to log in again!</strong>';
-           }           
-           ?>
+
+	if ($row->ID == 1) {
+		echo '<br/>Warning: <strong>This is the MAIN admin or site owner!</strong>';
+	}
+	if ($row->ID == $current_user->ID) {
+		echo '<br/>Warning: <strong>This is you! After changing your login name you might need to log in again!</strong>';
+	}
+?>
            </p>
              <form id="form1" name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF'].'?page='.$_GET['page'].'&user_id='.$row->ID; ?>" >
                <div class="plugin_admin_renamer-box01">
@@ -93,7 +106,8 @@ if (count($_POST)) {
            </div>
    </div>
    <?php
-   }
-   ?>
+
+}
+?>
 </body>
 </html>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://donate.ramonfincken.com
 Tags: admin,admins,rename,founder,change,user,users,renamer,extended
 Requires at least: 2.0.2
 Tested up to: 2.8.4
-Stable tag: 1.2
+Stable tag: 1.3
 
 Plugin to change your default admin username ( with GUI to change all other admin names too ).
 
@@ -30,6 +30,19 @@ First release
 
 = 1.2 =
 Tidying of html code, added screenshots
+
+= 1.3 =
+Applied WP way of handling usernames:
+
+if (empty ($newName)) {
+      $message = stripslashes($oldName) . ' NOT changed-> New name was empty!';
+      $showMsg = 'block';
+   } else {
+      if (!validate_username($newName)) {
+         $message = stripslashes($oldName) . ' NOT changed -> This username is invalid. Please enter a valid username.';
+         $showMsg = 'block';
+      }
+      [rest of code]
 
 == Screenshots ==
 
