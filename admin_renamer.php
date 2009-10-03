0f125eb5ba2ba9e6008faf7782903470177f883a
ramon fincken <ramon fincken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-03 21:03:17 +0000

    1.4 (1.3 actually)
    git-svn-id: https://plugins.svn.wordpress.org/admin-renamer-extended/tags/1.4@160268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin_renamer.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Admin renamer extended
+Plugin URI: http://www.websitefreelancers.nl
+Description: Lets you rename all admin usernames with GUI.
+Version: 1.4
+Author: Ramon Fincken
+Author URI: http://www.websitefreelancers.nl
+*/
+
+if (!defined('PLUGIN_ADMIN_RENAMER_DIR')) {
+   define('PLUGIN_ADMIN_RENAMER_DIR', WP_PLUGIN_DIR . '/' . plugin_basename(dirname(__FILE__)));
+}
+
+if (!defined('PLUGIN_ADMIN_RENAMER_BASENAME')) {
+   define('PLUGIN_ADMIN_RENAMER_BASENAME', plugin_basename(__FILE__));
+}
+
+// Admin only :)
+if (is_admin()) {
+   require_once PLUGIN_ADMIN_RENAMER_DIR . '/admin.php';
+}
+?>
